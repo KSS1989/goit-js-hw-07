48c0ced21a04447a7120d541c61c0a7285c57e01
KSS <99805859+KSS1989@users.noreply.github.com>
2023-02-25 20:52:03 +0200

    +

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,48 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const item = [];
+
+galleryItems.forEach((el) => {
+  const galleryItem = document.createElement("div");
+  galleryItem.className = "gallery__item";
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__link";
+  galleryLink.href = el.original;
+  const galleryImg = document.createElement("img");
+  galleryImg.className = "gallery__image";
+  galleryImg.src = el.preview;
+  galleryImg.setAttribute("data-source", el.original);
+  galleryImg.alt = el.description;
+
+  galleryItem.append(galleryLink);
+  galleryLink.append(galleryImg);
+  item.push(galleryItem);
+});
+gallery.append(...item);
+
+gallery.addEventListener("click", (elm) => {
+  elm.preventDefault();
+  if (elm.target.nodeName !== "IMG") {
+    return;
+  }
+
+  const selectedImage = elm.target.getAttribute("data-source");
+
+  const instance = basicLightbox.create(`
+    <img src="${selectedImage}" width="800" height="600">
+`);
+
+  instance.show();
+
+  gallery.addEventListener("keydown", (elm) => {
+    if (elm.key === "Escape") {
+      instance.close();
+    }
+  });
+});
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const gallery = document.querySelector(".gallery");
+console.log(gallery);
+
+const item = [];
+
+galleryItems.forEach((el) => {
+  const galleryLink = document.createElement("a");
+  galleryLink.className = "gallery__item";
+  galleryLink.href = el.original;
+
+  const galleryImg = document.createElement("img");
+  galleryImg.className = "gallery__image";
+  galleryImg.src = el.preview;
+  galleryImg.setAttribute("title", el.description);
+  galleryImg.alt = el.description;
+
+  galleryLink.append(galleryImg);
+  item.push(galleryLink);
+});
+gallery.append(...item);
+
+new SimpleLightbox(".gallery a", {
+  captionDelay: 250,
+});
